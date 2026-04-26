51ae3a2cc5fc3e0b76b6e90951fc9450bd1e6626
SashaGS <Fqva@ukr.net>
2026-04-26 20:45:52 +0300

    done hw-js-04

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,18 @@
-function slugify(title) {
-    let slug = title.toLowerCase().split(" ");
-    return slug.join("-");
+function isEnoughCapacity(products, containerSize) {
+    let sumInProdact = 0;
+    let prodVal = Object.values(products);
+
+    for (const item of prodVal) {
+        sumInProdact += item;
+    }
+    if (sumInProdact<=containerSize) {
+        return true;
+    } else {
+        return false; 
+    }
 }
  
-console.log(slugify("Arrays for beginners")); 
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); 
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
